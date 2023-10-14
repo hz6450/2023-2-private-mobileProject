4b41f6989357be43ceab0554eea84c6b092a295f
hz6450 <59460811+hz6450@users.noreply.github.com>
2023-10-14 12:45:36 +0900

    캘린더 기능 수정

--- src/screens/SecondScreen.js
+++ src/screens/SecondScreen.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { View, TextInput, Button, Picker } from "react-native";
+import { View, TextInput, TouchableOpacity, StyleSheet, Picker  } from "react-native";
 import { Layout, TopNav, Text, themeColor, useTheme } from "react-native-rapi-ui";
 import { Ionicons } from "@expo/vector-icons";
 import { initializeApp, getApps } from "firebase/app";
-import { getFirestore, doc, getDoc, updateDoc, arrayUnion } from "firebase/firestore";
+import { getFirestore, doc, getDoc, updateDoc } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import firebaseConfig from "../firebaseConfig";
 import { Calendar } from "react-native-calendars";
+import { addMonths, subMonths, format } from "date-fns";
 
 export default function ({ navigation }) {
   if (getApps().length === 0) {
@@ -21,11 +22,10 @@ export default function ({ navigation }) {
   const [selectedDate, setSelectedDate] = useState("");
   const [text, setText] = useState("");
   const [selectedTime, setSelectedTime] = useState("00:00");
-
+  const [currentMonth, setCurrentMonth] = useState(new Date());
   const [userUID, setUserUID] = useState("");
   const [userData, setUserData] = useState([]);
 
-  // Firebase Authentication에서 사용자 UID 가져오기
   const auth = getAuth();
   onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -33,7 +33,6 @@ export default function ({ navigation }) {
     }
   });
 
-  // 사용자 데이터를 가져오는 함수
   const fetchUserData = async () => {
     if (userUID && selectedDate) {
       try {
@@ -57,7 +56,6 @@ export default function ({ navigation }) {
   const saveData = async () => {
     if (userUID && selectedDate && text) {
       try {
-        // Firestore에 데이터 저장
         const userDataRef = doc(db, "users", userUID);
         const updatedData = userData.slice();
         updatedData.push({ date: selectedDate, time: selectedTime, value: text });
@@ -81,67 +79,125 @@ export default function ({ navigation }) {
     return "No data";
   };
 
+  const calculateMarkedDates = () => {
+    const markedDates = {};
+
+    userData.forEach((item) => {
+      markedDates[item.date] = { marked: true };
+    });
+
+    return markedDates;
+  };
+
+  const markedDates = calculateMarkedDates();
+
+  const handleNextMonth = () => {
+    setCurrentMonth(addMonths(currentMonth, 1));
+  };
+
+  const handlePrevMonth = () => {
+    setCurrentMonth(subMonths(currentMonth, 1));
+  };
+
   return (
-    <Layout>
+    <Layout style={styles.container}>
       <TopNav
         middleContent="오늘 할 일"
-        leftContent={
-          <Ionicons
-            name="chevron-back"
-            size={20}
-            color={isDarkmode ? themeColor.white100 : themeColor.dark}
-          />
-        }
+        leftContent={<Ionicons name="chevron-back" size={20} color={isDarkmode ? themeColor.white100 : themeColor.dark} />}
         leftAction={() => navigation.goBack()}
-        rightContent={
-          <Ionicons
-            name={isDarkmode ? "sunny" : "moon"}
-            size={20}
-            color={isDarkmode ? themeColor.white100 : themeColor.dark}
-          />
-        }
-        rightAction={() => {
-          if (isDarkmode) {
-            setTheme("light");
-          } else {
-            setTheme("dark");
-          }
-        }}
+        rightContent={<Ionicons name={isDarkmode ? "sunny" : "moon"} size={20} color={isDarkmode ? themeColor.white100 : themeColor.dark} />}
+        rightAction={() => isDarkmode ? setTheme("light") : setTheme("dark")}
       />
+
+      <View style={styles.calendarHeader}>
+        <TouchableOpacity onPress={handlePrevMonth}>
+          <Text>{"< Previous"}</Text>
+        </TouchableOpacity>
+        <Text>{format(currentMonth, "MMMM yyyy")}</Text>
+        <TouchableOpacity onPress={handleNextMonth}>
+          <Text>{"Next >"}</Text>
+        </TouchableOpacity>
+      </View>
 
       <Calendar
-        onDayPress={(day) => {
-          setSelectedDate(day.dateString);
-        }}
-      />
+  key={format(currentMonth, "yyyy-MM-dd")} // key prop을 추가했습니다.
+  style={styles.calendar}
+  current={format(currentMonth, "yyyy-MM-dd")}
+  onDayPress={(day) => setSelectedDate(day.dateString)}
+  markedDates={markedDates}
+/>
 
-      <Picker
-        selectedValue={selectedTime}
-        onValueChange={(itemValue, itemIndex) => setSelectedTime(itemValue)}
-      >
-        {Array.from({ length: 49 }, (_, i) => (
-          <Picker.Item
-            key={i}
-            label={`${String(Math.floor(i / 2)).padStart(2, "0")}:${i % 2 === 0 ? "00" : "30"}`}
-            value={`${String(Math.floor(i / 2)).padStart(2, "0")}:${i % 2 === 0 ? "00" : "30"}`}
-          />
-        ))}
-      </Picker>
+      <View style={styles.pickerContainer}>
+        <Picker
+          style={styles.picker}
+          selectedValue={selectedTime}
+          onValueChange={(itemValue, itemIndex) => setSelectedTime(itemValue)}
+        >
+          {Array.from({ length: 49 }, (_, i) => (
+            <Picker.Item
+              key={i}
+              label={`${String(Math.floor(i / 2)).padStart(2, "0")}:${i % 2 === 0 ? "00" : "30"}`}
+              value={`${String(Math.floor(i / 2)).padStart(2, "0")}:${i % 2 === 0 ? "00" : "30"}`}
+            />
+          ))}
+        </Picker>
+      </View>
 
       <TextInput
+        style={styles.input}
         value={text}
         onChangeText={(newText) => setText(newText)}
-        placeholder="Enter text here"
       />
 
-      <Button
-        title="Save Data"
-        onPress={saveData}
-        disabled={!selectedDate || !text}
-      />
+      <TouchableOpacity onPress={saveData} style={styles.saveButton}>
+        <Text style={styles.saveButtonText}>Save</Text>
+      </TouchableOpacity>
 
-      {/* 사용자 데이터 출력 */}
-      <Text>{`오늘 할 일:\n${getSelectedDateValues()}`}</Text>
+      <Text style={styles.userDataText}>{`오늘 할 일:\n${getSelectedDateValues()}`}</Text>
     </Layout>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  calendar: {
+    marginBottom: 20,
+  },
+  pickerContainer: {
+    flexDirection: "row",
+    marginBottom: 20,
+  },
+  picker: {
+    flex: 1,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  saveButton: {
+    backgroundColor: "#2196F3",
+    padding: 10,
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  saveButtonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  userDataText: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  calendarHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+});
