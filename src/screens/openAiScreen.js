d47303838820f61ce132c6d577b1079c86765210
hz6450 <59460811+hz6450@users.noreply.github.com>
2023-10-17 09:56:02 +0900

    openAiScreen 수정필요

--- src/screens/openAiScreen.js
+++ src/screens/openAiScreen.js
@@ -1,41 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import { View, ScrollView } from 'react-native';
+import { View } from 'react-native';
 import { TopNav, themeColor, useTheme, Text, Button } from 'react-native-rapi-ui';
 import { Ionicons } from '@expo/vector-icons';
-import { generateOpenAIResponse } from './openaiService'; // OpenAI API 호출을 위한 서비스 파일
-import { getFirestore, doc, getDoc } from 'firebase/firestore'; // Firebase Firestore 관련 import 추가
+import { generateOpenAIResponse } from './openaiService';
+import { getFirestore, query, where, collection, getDocs, doc, getDoc  } from 'firebase/firestore';
+
+
 
 export default function OpenAIScreen({ navigation }) {
   const { isDarkmode, setTheme } = useTheme();
-  const [selectedDate, setSelectedDate] = useState('');
+  const [selectedMonth, setSelectedMonth] = useState(new Date().toISOString().slice(0, 7)); // YYYY-MM 형식으로 초기 월 설정
   const [userData, setUserData] = useState([]);
   const [openAIResponse, setOpenAIResponse] = useState('');
-  const db = getFirestore(); // Firestore 인스턴스 생성
+  const db = getFirestore();
 
-  // Firebase로부터 사용자 데이터 가져오기 (이 부분은 이전 코드와 동일)
+  const moveMonth = (offset) => {
+    const date = new Date(selectedMonth);
+    date.setMonth(date.getMonth() + offset);
+    setSelectedMonth(date.toISOString().slice(0, 7));
+  };
+
   const fetchUserData = async () => {
-    if (selectedDate) {
+    if (selectedMonth) {
       try {
-        const userDataRef = doc(db, 'users', selectedDate); // 'users' 컬렉션에서 선택한 날짜의 문서 가져오기
+        const userDataRef = doc(db, 'users', '8RE0UL0x7gIlNgpKTeE83jQZLa2'); // 문서 ID를 고정값으로 사용함 (변경 필요)
         const userDataDoc = await getDoc(userDataRef);
-
+  
         if (userDataDoc.exists()) {
-          const userData = userDataDoc.data();
-          setUserData(userData.data || []);
+          // 원래 데이터를 가져옵니다.
+          const originalData = userDataDoc.data().data || [];
+          // date 필드를 수정합니다.
+          const modifiedData = originalData.map(item => ({
+            ...item,
+            date: item.date.replace(/-/g, '')
+          }));
+          // 선택된 월의 데이터만 필터링합니다.
+          const filteredData = modifiedData.filter(item => item.date.startsWith(selectedMonth.replace(/-/g, '')));
+  
+          // 필터링된 데이터를 상태로 설정합니다.
+          setUserData(filteredData);
         } else {
-          setUserData([]); // 데이터가 없는 경우 빈 배열로 설정
+          setUserData([]);
+          console.warn(`No data found for month: ${selectedMonth}`);
         }
       } catch (error) {
-        console.error('데이터 가져오기 중 오류 발생:', error);
+        console.error('Error fetching data:', error);
       }
     }
   };
+  
+  
+  
 
   useEffect(() => {
     fetchUserData();
-  }, [selectedDate]);
+  }, [selectedMonth]);
 
-  // OpenAI API를 호출하여 추천 일정을 생성하는 함수
   const generateRecommendation = async () => {
     try {
       // generateOpenAIResponse는 OpenAI API를 호출하여 추천 일정을 생성하는 함수입니다.
@@ -44,17 +64,6 @@ export default function OpenAIScreen({ navigation }) {
     } catch (error) {
       console.error('OpenAI API 호출 중 오류 발생:', error);
     }
-  };
-
-  // 날짜별 할 일을 컨테이너로 구분하여 표시
-  const renderUserDataContainers = () => {
-    return userData.map((item, index) => (
-      <View  key={index} style={{ margin: 8 }}>
-        <Text>Date: {item.date}</Text>
-        <Text>Task: {item.task}</Text>
-        {/* 기타 필요한 데이터 표시 */}
-      </View >
-    ));
   };
 
   return (
@@ -67,22 +76,25 @@ export default function OpenAIScreen({ navigation }) {
         rightAction={() => isDarkmode ? setTheme('light') : setTheme('dark')}
       />
 
-      {/* 날짜 선택 */}
-      <Text>Select a date: {selectedDate}</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+        <Button title="Prev Month" onPress={() => moveMonth(-1)} />
+        <Text style={{ marginHorizontal: 10 }}>{selectedMonth}</Text>
+        <Button title="Next Month" onPress={() => moveMonth(1)} />
+      </View>
 
       {/* Firebase로부터 가져온 사용자 데이터 표시 */}
       {userData.map((item, index) => (
-        <View key={index} padding-10 marginT-10 bg-white radius-10>
+        <View key={index} style={{ padding: 10, marginTop: 10, backgroundColor: 'white', borderRadius: 10 }}>
           <Text>할 일: {item.value}</Text>
           <Text>시간: {item.time}</Text>
         </View>
       ))}
 
-      {/* 추천 일정 생성 버튼 */}
-      <Button
+       {/* 추천 일정 생성 버튼 */}
+       <Button
         title="Generate Recommendation"
         onPress={generateRecommendation}
-        disabled={!selectedDate || userData.length === 0}
+        disabled={!selectedMonth || userData.length === 0}
       />
 
       {/* OpenAI API에서 생성한 추천 일정 표시 */}
