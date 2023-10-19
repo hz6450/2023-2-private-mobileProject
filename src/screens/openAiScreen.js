c26929d77a560e1cb2bc3498562bc1a6c5d87db4
hz6450 <59460811+hz6450@users.noreply.github.com>
2023-10-19 11:19:03 +0900

    openai 구현

--- src/screens/openAiScreen.js
+++ src/screens/openAiScreen.js
@@ -4,53 +4,77 @@ import { TopNav, themeColor, useTheme, Text, Button } from 'react-native-rapi-ui
 import { Ionicons } from '@expo/vector-icons';
 import { generateOpenAIResponse } from './openaiService';
 import { getFirestore, query, where, collection, getDocs, doc, getDoc  } from 'firebase/firestore';
-
-
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export default function OpenAIScreen({ navigation }) {
   const { isDarkmode, setTheme } = useTheme();
   const [selectedMonth, setSelectedMonth] = useState(new Date().toISOString().slice(0, 7)); // YYYY-MM 형식으로 초기 월 설정
-  const [userData, setUserData] = useState([]);
+  const [userData, setUserData] = useState({});
   const [openAIResponse, setOpenAIResponse] = useState('');
+  const [userUID, setUserUID] = useState(null); // 사용자 UID를 위한 상태 추가
   const db = getFirestore();
 
   const moveMonth = (offset) => {
     const date = new Date(selectedMonth);
     date.setMonth(date.getMonth() + offset);
     setSelectedMonth(date.toISOString().slice(0, 7));
+    
   };
 
   const fetchUserData = async () => {
-    if (selectedMonth) {
+    if (userUID && selectedMonth) {
       try {
-        const userDataRef = doc(db, 'users', '8RE0UL0x7gIlNgpKTeE83jQZLa2'); // 문서 ID를 고정값으로 사용함 (변경 필요)
+        const userDataRef = doc(db, 'users', userUID);
         const userDataDoc = await getDoc(userDataRef);
   
         if (userDataDoc.exists()) {
-          // 원래 데이터를 가져옵니다.
           const originalData = userDataDoc.data().data || [];
-          // date 필드를 수정합니다.
-          const modifiedData = originalData.map(item => ({
-            ...item,
-            date: item.date.replace(/-/g, '')
-          }));
-          // 선택된 월의 데이터만 필터링합니다.
-          const filteredData = modifiedData.filter(item => item.date.startsWith(selectedMonth.replace(/-/g, '')));
   
-          // 필터링된 데이터를 상태로 설정합니다.
-          setUserData(filteredData);
+          const groupedData = {};
+  
+          originalData.forEach(item => {
+            const month = item.date.slice(0, 7);
+            const day = item.date; // 일자를 포함한 날짜 YYYY-MM-DD
+  
+            if (!groupedData[month]) {
+              groupedData[month] = {};
+            }
+  
+            if (!groupedData[month][day]) {
+              groupedData[month][day] = [];
+            }
+  
+            groupedData[month][day].push({
+              date: item.date,
+              value: item.value,
+              time: item.time
+            });
+          });
+  
+          console.log("Fetched User Data:", groupedData);
+  
+          setUserData(groupedData);
         } else {
-          setUserData([]);
           console.warn(`No data found for month: ${selectedMonth}`);
         }
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('데이터 가져오기 중 오류 발생:', error);
       }
     }
   };
-  
-  
-  
+useEffect(() => {
+  const auth = getAuth();
+
+  const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+          setUserUID(user.uid);
+      }
+  });
+
+  // 컴포넌트 언마운트시 정리 함수 호출
+  return () => unsubscribe();
+}, []); // 빈 의존성 배열을 사용하여 컴포넌트가 마운트될 때만 실행되도록 합니다.
+
 
   useEffect(() => {
     fetchUserData();
@@ -58,13 +82,17 @@ export default function OpenAIScreen({ navigation }) {
 
   const generateRecommendation = async () => {
     try {
-      // generateOpenAIResponse는 OpenAI API를 호출하여 추천 일정을 생성하는 함수입니다.
-      const response = await generateOpenAIResponse(selectedDate, userData);
-      setOpenAIResponse(response); // API 응답을 상태에 저장
+      const dataForSelectedMonth = Object.values(userData[selectedMonth] || {});
+      const flattenedData = dataForSelectedMonth.flat();
+const response = await generateOpenAIResponse(flattenedData);
+
+  
+      setOpenAIResponse(response);
     } catch (error) {
       console.error('OpenAI API 호출 중 오류 발생:', error);
     }
   };
+  
 
   return (
     <View>
@@ -83,22 +111,30 @@ export default function OpenAIScreen({ navigation }) {
       </View>
 
       {/* Firebase로부터 가져온 사용자 데이터 표시 */}
-      {userData.map((item, index) => (
-        <View key={index} style={{ padding: 10, marginTop: 10, backgroundColor: 'white', borderRadius: 10 }}>
-          <Text>할 일: {item.value}</Text>
-          <Text>시간: {item.time}</Text>
-        </View>
-      ))}
-
-       {/* 추천 일정 생성 버튼 */}
-       <Button
-        title="Generate Recommendation"
-        onPress={generateRecommendation}
-        disabled={!selectedMonth || userData.length === 0}
-      />
+       {/* Firebase로부터 가져온 사용자 데이터 표시 */}
+    {Object.entries(userData[selectedMonth] || {}).map(([day, items]) => (
+      <View key={day} style={{ padding: 10, marginTop: 10, backgroundColor: 'white', borderRadius: 10 }}>
+        <Text>{day}</Text>
+        {items.map((item, index) => (
+          <View key={index}>
+            <Text>할 일: {item.value}</Text>
+            <Text>시간: {item.time}</Text>
+          </View>
+        ))}
+      </View>
+    ))}
 
       {/* OpenAI API에서 생성한 추천 일정 표시 */}
-      <Text>Recommendation: {openAIResponse}</Text>
+      {openAIResponse && (
+  <View style={{ padding: 10, marginTop: 10, backgroundColor: 'white', borderRadius: 10 }}>
+    <Text>오늘의 일정 요약: {openAIResponse}</Text>
+  </View>
+)}
+<Button
+  title="내일의 추천 일정 생성"
+  onPress={generateRecommendation}
+  disabled={!selectedMonth || userData.length === 0}
+/>
     </View>
   );
 }
--- src/screens/openaiService.js
+++ src/screens/openaiService.js
@@ -1,34 +1,43 @@
 import axios from 'axios';
 
-// OpenAI API 엔드포인트 URL
-const OPENAI_API_URL = 'https://api.openai.com/v1/your_endpoint_here'; // OpenAI API 엔드포인트 URL을 여기에 추가
+const OPENAI_API_URL = 'https://api.openai.com/v1/engines/davinci/completions';
+const OPENAI_API_KEY = ''; // 실제 API 키 사용 시, 이를 서버나 환경 변수에서 안전하게 관리하세요.
 
-// OpenAI API 키 (인증 헤더에 사용됨)
-const OPENAI_API_KEY = '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'; // OpenAI API 키를 여기에 추가
 
-// OpenAI API를 호출하여 추천 일정을 생성하는 함수
-export async function generateOpenAIResponse(selectedDate, userData) {
+export async function generateOpenAIResponse(userData) {
   try {
-    // OpenAI API 요청 본문 데이터 구성
-    const requestData = {
-      prompt: `Generate a schedule for ${selectedDate} based on the following tasks:\n${userData.map(item => item.value).join('\n')}`,
-      max_tokens: 50, // 생성된 텍스트의 최대 길이
-    };
+      // userData가 배열인지 확인합니다.
+      if (!Array.isArray(userData)) {
+          throw new Error("Invalid userData. Expected an array.");
+      }
 
-    // OpenAI API 요청 헤더 구성
-    const requestHeaders = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${OPENAI_API_KEY}`,
-    };
+        // 사용자 데이터를 바탕으로 쿼리를 생성합니다.
+        let prompt = `오늘의 사용자의 스케줄을 기반으로 일정을 요약하고 새로운 이벤트 추천을 해주세요.\n\n`;
+        userData.forEach(event => {
+            prompt += `이벤트: ${event.value}, 시간: ${event.time}\n`;
+        });
+        prompt += "\n요약 및 추천:";
 
-    // OpenAI API 호출
-    const response = await axios.post(OPENAI_API_URL, requestData, { headers: requestHeaders });
+        const response = await axios.post(OPENAI_API_URL, {
+            prompt: prompt,
+            max_tokens: 150,
+            temperature: 0.5
+        }, {
+            headers: {
+                'Authorization': `Bearer ${OPENAI_API_KEY}`,
+                'Content-Type': 'application/json'
+            }
+        });
 
-    // API 응답에서 생성된 일정 추천 추출
-    const recommendation = response.data.choices[0]?.text || 'No recommendation available';
-
-    return recommendation;
-  } catch (error) {
-    throw error;
-  }
+        // OpenAI의 응답에서 요약 및 추천된 일정 텍스트를 추출합니다.
+        if (response.data.choices && response.data.choices.length > 0) {
+            const summaryAndRecommendation = response.data.choices[0].text.trim();
+            return summaryAndRecommendation;
+        } else {
+            throw new Error("No summary or recommendation received from OpenAI.");
+        }
+    } catch (error) {
+        console.error('Error calling OpenAI:', error);
+        throw error;
+    }
 }
